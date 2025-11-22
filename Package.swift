c31af2564e27ca5c69713af60eca15864e39c3db
jiangshulun <18610905033@163.com>
2025-11-22 13:39:45 +0800

    更新SwiftBasisKit中ShareKit相关配置

--- Package.swift
+++ Package.swift
@@ -18,7 +18,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftBasisKit"),
+            name: "SwiftBasisKit",
+            dependencies: [
+                .product(name: "SwiftShareKit", package: "SwiftShareKit")
+            ]),
         .testTarget(
             name: "SwiftBasisKitTests",
             dependencies: ["SwiftBasisKit"]
